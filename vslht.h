51634c46c5a3a56a76a36db0ba914d0bdd67a539
Tim Leek <tleek@ll.mit.edu>
2009-03-25 12:55:17 -0400

    Looks okay too

--- vslht.h
+++ vslht.h
@@ -12,20 +12,21 @@
    The table is little more than a pair of arrays: keys and values. 
 */
 typedef struct vslht {
-  uint32_t size;   ///< num slots in table
-  uint32_t occ;    ///< number of actual key/value pairs 
-  char **key;      ///< array of strings that are keys
-  uint32_t *val;   ///< array of ints that are values
+  uint32_t size;   // num slots in table
+  uint32_t occ;    // number of actual key/value pairs 
+  char **key;      // array of strings that are keys
+  uint64_t *val;   // array of ints that are values
+  char *del;   // lazy deletion
 } vslht;
 
 /// API Functions
 vslht *vslht_new (void);
 void vslht_clear (vslht *h);
 void vslht_free (vslht *h);
-void vslht_add (vslht *h, char *k, uint32_t v);
+void vslht_add (vslht *h, char *k, uint64_t v);
 void vslht_remove (vslht *h, char *k);
 uint32_t vslht_mem(vslht *h, char *k);
-uint32_t vslht_find (vslht *h, char *k);
+uint64_t vslht_find (vslht *h, char *k);
 void vslht_copy (vslht *h1, vslht *h2);
 char **vslht_key_set (vslht *h);
 uint32_t vslht_size(vslht *h);
