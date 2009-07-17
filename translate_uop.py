7dfa9af53a30f1ece57418841c016d2af1c5e2c4
dolan <dolan@laptop1>
2009-07-17 13:07:12 -0400

    Add new translation functions to support EnumProcesses

--- translate_uop.py
+++ translate_uop.py
@@ -3,6 +3,13 @@
 import sys
 from qemu_data import *
 
+width = {
+    0: 'B',
+    1: 'H',
+    2: 'L',
+    3: 'Q',
+}
+
 op_handler = {
     "IFLO_LABEL_OUTPUT": lambda args: "print mem.read(%#x,%d).encode('hex')" % (args[0],args[1]),
     "IFLO_OPREG_TEMPL_ADDL_A0_R": lambda args: "A0 += %s" % qemu_regs_r[args[0]],
@@ -10,10 +17,13 @@ op_handler = {
     "IFLO_OPREG_TEMPL_MOVL_T0_R": lambda args: "T0 = %s" % qemu_regs_r[args[0]],
     "IFLO_OPREG_TEMPL_MOVL_T1_R": lambda args: "T1 = %s" % qemu_regs_r[args[0]],
     "IFLO_OPS_MEM_LDL_T0_A0": lambda args: "T0 = ULInt32(mem.read(A0,4))",
+    "IFLO_OPS_MEM_LDL_T1_A0": lambda args: "T1 = ULInt32(mem.read(A0,4))",
     "IFLO_OPS_MEM_LDUW_T0_A0": lambda args: "T0 = ULInt16(mem.read(A0,2))",
     "IFLO_OPS_MEM_STL_T0_A0": lambda args: "mem.write(A0,T0,'L')",
+    "IFLO_OPS_MEM_STW_T0_A0": lambda args: "mem.write(A0,T0,'H')",
     "IFLO_OPREG_TEMPL_MOVL_A0_R": lambda args: "A0 = %s" % qemu_regs_r[args[0]],
     "IFLO_OPREG_TEMPL_MOVL_R_A0": lambda args: "%s = A0" % qemu_regs_r[args[0]],
+    "IFLO_OPREG_TEMPL_MOVW_R_T0": lambda args: "%s = T0 & 0xFFFF" % qemu_regs_r[args[0]],
     "IFLO_MOVL_A0_IM": lambda args: "A0 = UInt(%#x)" % args[0],
     "IFLO_MOVL_T0_IM": lambda args: "T0 = UInt(%#x)" % args[0],
     "IFLO_MOVL_T1_IM": lambda args: "T1 = UInt(%#x)" % args[0],
@@ -31,16 +41,37 @@ op_handler = {
     "IFLO_OPS_TEMPLATE_MOVL_T0_DSHIFT": lambda args: "T0 = DF << %d" % args[0],
     "IFLO_OPS_TEMPLATE_SHR_T0_T1": lambda args: "T0 = T0 >> T1",
     "IFLO_OPS_TEMPLATE_SHL_T0_T1": lambda args: "T0 = T0 << T1",
+    "IFLO_OPS_TEMPLATE_SHL_T0_T1_CC": lambda args: "T0 = T0 << T1",
+    "IFLO_OPS_TEMPLATE_SHR_T0_T1_CC": lambda args: "T0 = T0 >> T1",
     "IFLO_OPS_MEM_LDUB_T0_A0": lambda args: "T0 = ULInt8(mem.read(A0,1))",
     "IFLO_OPREG_TEMPL_MOVB_R_T0": lambda args: "%s = T0 & 0xFF" % qemu_regs_r[args[0]],
     "IFLO_OPS_MEM_STB_T0_A0": lambda args: "mem.write(A0,T0,'B')",
-    "IFLO_OPS_TEMPLATE_JNZ_ECX": lambda args: "ECX != 0",
     "IFLO_TB_HEAD_EIP": lambda args: "",
     "IFLO_GOTO_TB0": lambda args: "",
     "IFLO_SET_INPUT": lambda args: "%s = inputs[%d]" % (args[0], args[1]),
+    "IFLO_OPREG_TEMPL_ADDL_A0_R_S1": lambda args: "A0 = A0 + (%s << 1)" % qemu_regs_r[args[0]],
+    "IFLO_OPREG_TEMPL_ADDL_A0_R_S2": lambda args: "A0 = A0 + (%s << 2)" % qemu_regs_r[args[0]],
+    "IFLO_OPREG_TEMPL_ADDL_A0_R_S3": lambda args: "A0 = A0 + (%s << 3)" % qemu_regs_r[args[0]],
+    "IFLO_ANDL_T0_T1": lambda args: "T0 &= T1",
+    "IFLO_ORL_T0_T1": lambda args: "T0 |= T1",
+    "IFLO_XOR_T0_1": lambda args: "T0 ^= 1",
+    "IFLO_DECL_T0": lambda args: "T0 -= 1",
+    "IFLO_MOVL_T0_0": lambda args: "T0 = UInt(0)",
+    "IFLO_NEGL_T0": lambda args: "T0 *= -1",
+    "IFLO_OPS_TEMPLATE_SHR_T0_T1_CC_MEMWRITE": lambda args: "T0 = T0 >> T1\nmem.write(A0,T0,'%s')" % width[args[0]],
+    "IFLO_MOVZWL_T0_T0": lambda args: "T0 = UInt(UShort(T0))",
+    "IFLO_MOVZBL_T0_T0": lambda args: "T0 = UInt(Byte(T0))",
+    "IFLO_OPS_TEMPLATE_SETZ_T0_SUB": lambda args: "T0 = UInt(%#x)" % args[1],
+    "IFLO_OPS_TEMPLATE_SBB_T0_T1_CC": lambda args: "T0 = T0 - T1",
+    "IFLO_CMPXCHG8B_PART1": lambda args: "mem.write(A0, ECX << 32 | EBX, 'Q')",
+    "IFLO_OPS_TEMPLATE_SAR_T0_T1": lambda args: "T0 = T0 >> T1",
+    "IFLO_TESTL_T0_T1_CC": lambda args: "CC_DST = T0 & T1",
 
     "IFLO_ADDL_A0_SEG": lambda args: "A0 += %s" % fieldname(field_from_env(args[1])),
     "IFLO_MOVL_SEG_T0": lambda args: "%s = load_seg(mem, T0, GDT, LDT)" % qemu_segs_r[args[0]],
+
+    "IFLO_OPS_TEMPLATE_JNZ_ECX": lambda args: "ECX != 0",
+    "IFLO_OPS_TEMPLATE_JZ_SUB": lambda args: "CC_DST == 0",
 }
 
 outop_handler = {
