d00ebd9e88633cb0d192660742731fb346fe7e32
moyix <devnull@localhost>
2009-09-25 16:59:31 -0400

    Fix bugs and add compiled exe

--- EnumProcesses.c
+++ EnumProcesses.c
@@ -1,14 +1,18 @@
 #define __WIN32_LEAN_AND_MEAN__
 #include <windows.h>
+#include <psapi.h>
+#pragma comment(lib, "psapi.lib")
 #include <stdio.h>
 #include "vmnotify.h"
+
 
 int main(int argc, char **argv) {
   DWORD *pids;
   DWORD outcb;
-  buf = (DWORD *) malloc(256);
+  int i;
+  pids = (DWORD *) malloc(256);
   vm_mark_buf_in(&pids, 4);
-  EnumProcesses(pids, 256, outcb);
+  EnumProcesses(pids, 256, &outcb);
   vm_mark_buf_out(pids, 256);
   return 0;
 }
