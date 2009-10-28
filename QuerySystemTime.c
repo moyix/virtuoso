f46e7fd6e3e98e85a8520a918b4a3fd013b63b62
moyix <moyix@amnesia.gtisc.gatech.edu>
2009-10-28 18:59:35 -0400

    use LoadLibrary/GetProcAddress to get NtQuerySystemTime

--- QuerySystemTime.c
+++ QuerySystemTime.c
@@ -3,9 +3,19 @@
 #include <stdio.h>
 #include "vmnotify.h"
 
+typedef NTSTATUS (__stdcall *PFN_NtQuerySystemTime)(
+		OUT PLARGE_INTEGER SystemTime
+		);
+
+PFN_NtQuerySystemTime NtQuerySystemTime;
+
 int main(int argc, char **argv) {
   PLARGE_INTEGER SystemTime;
   SystemTime = (PLARGE_INTEGER) malloc(sizeof(LARGE_INTEGER));
+  if( !(NtQuerySystemTime = (PFN_NtQuerySystemTime) GetProcAddress( GetModuleHandle("ntdll.dll"),
+			"NtQuerySystemTime" )) ) {
+    return 1;
+  }
   vm_mark_buf_in(&SystemTime, sizeof(PLARGE_INTEGER));
   NtQuerySystemTime(SystemTime);
   vm_mark_buf_out(SystemTime, sizeof(DWORD));
