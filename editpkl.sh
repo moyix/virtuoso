3f88eee15755b5ca75c211bdc3757365aa136f7e
brendan <brendan@ubuntu>
2010-06-17 11:32:13 -0700

    Robustness of shell script and bugfix related to strip in repick.py

--- editpkl.sh
+++ editpkl.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+if [ -z $VISUAL ]; then
+    VISUAL=vim
+fi
+
 [ $# -eq 1 ] || (echo "usage: $0 <pickle>" && exit 1)
-TMPFILE=`mktemp -t pickle`
-TMPOUT=`mktemp -t pickle`
+TMPFILE=`mktemp -t pickle.XXXX`
+TMPOUT=`mktemp -t pickle.XXXX`
 depick.py "$1" > "$TMPFILE"
 $VISUAL "$TMPFILE"
 repick.py "$TMPFILE" > "$TMPOUT"
@@ -14,3 +18,4 @@ else
     exit 1
 fi
 rm -f "$TMPOUT"
+echo "Pickle edit completed successfully!"
--- repick.py
+++ repick.py
@@ -5,5 +5,5 @@ from cPickle import dump, HIGHEST_PROTOCOL
 
 execfile(sys.argv[1])
 for k in transdict:
-    transdict[k] = "\n".join(l.strip() for l in transdict[k].splitlines() if l.strip())
+    transdict[k] = "\n".join(l.rstrip() for l in transdict[k].splitlines() if l.rstrip())
 dump(transdict, sys.stdout, protocol=HIGHEST_PROTOCOL)
