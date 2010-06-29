eb73d0491dc83c1a93c3bf1bc0131d8af4faa1d3
Brendan Dolan-Gavitt <brendan@cc.gatech.edu>
2010-06-29 10:12:15 -0400

    Add edge labels to CFG diagram

--- cfgtools.py
+++ cfgtools.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def nodename(n):
     if isinstance(n,str):
         return "n_" + n.replace("_","")
@@ -10,15 +12,19 @@ def nodestr(n):
     else:
         return "%#x" % n
 
-def make_cfg_dot(cfg,tbdict,fname):
+def make_cfg_dot(tbs,tbdict,fname):
     of = open(fname,'w')
     print >>of, "digraph G {"
     print >>of, "node [shape=record]"
-    
-    nodeset = set(c for c in cfg)
-    for c in cfg:
-        nodeset.update(cfg[c])
-    
+
+    nodeset = set()
+    edges = defaultdict(int)
+
+    for i in range(len(tbs)-1):
+        nodeset.add(tbs[i].label)
+        edges[tbs[i].label,tbs[i+1].label] += 1
+    nodeset.add(tbs[i+1].label)
+
     for n in nodeset:
         if any(t.has_slice() for t in tbdict[n]):
             filled = 'style="filled" '
@@ -26,9 +32,8 @@ def make_cfg_dot(cfg,tbdict,fname):
             filled = ''
         print >>of, '%s [ label="%s" %s]' % (nodename(n),nodestr(n),filled)
     
-    for c in cfg:
-        for t in cfg[c]:
-            print >>of, '%s -> %s' % (nodename(c),nodename(t))
+    for c,t in edges:
+        print >>of, '%s -> %s [label="%d"]' % (nodename(c),nodename(t),edges[c,t])
     
     print >>of,"}"
     of.close()
