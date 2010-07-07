fa1cc9018dd7e745ce45f42338e0b72de125676b
brendandg <brendandg@professorcalculus.sst.ll.mit.edu>
2010-07-07 16:11:40 -0400

    Change to using LoadLibrary for iphlpapi

--- GetTcpTable.c
+++ GetTcpTable.c
@@ -2,20 +2,26 @@
 #include <windows.h>
 
 #include <Iphlpapi.h>
-#pragma comment(lib, "Iphlpapi")
 
 #include "vmnotify.h"
+#pragma comment(lib, "Ws2_32")
 
+typedef DWORD (*myGetTcpInfo)(PVOID, PDWORD, BOOL, ULONG, TCP_TABLE_CLASS, ULONG);
 
 int main(int argc, char **argv) {
     unsigned char *buf;
-    int status;
+    int status, x;
+    MIB_TCPTABLE_OWNER_PID *ret;
+    myGetTcpInfo gettcp;
+    HMODULE iphlp;
     DWORD dwSize = 0x1000;
 
     buf = (unsigned char *) malloc(0x1000);
     
     vm_mark_buf_in(&buf, 4);
-    status = GetExtendedTcpTable(buf,                        // pTcpTable
+    iphlp = LoadLibrary("iphlpapi.dll");
+    gettcp = (myGetTcpInfo) GetProcAddress(iphlp, "GetExtendedTcpTable");
+    status =     gettcp(buf,                        // pTcpTable
                         &dwSize,                    // pdwSize
                         FALSE,                      // bOrder
                         AF_INET,                    // ulAf
@@ -25,5 +31,16 @@ int main(int argc, char **argv) {
     
     printf(status == NO_ERROR ? "Success\n" : "Fail\n");
     
+    ret = (MIB_TCPTABLE_OWNER_PID *)buf;
+    for(x = 0; x < ret->dwNumEntries; x++) {
+        printf("Pid: %4d %15s : %-6d -> %15s : %-6d\n",
+                ret->table[x].dwOwningPid,
+                inet_ntoa(*((struct in_addr *) &ret->table[x].dwLocalAddr)),
+                ntohs(ret->table[x].dwLocalPort),
+                inet_ntoa(*((struct in_addr *) &ret->table[x].dwRemoteAddr)),
+                ntohs(ret->table[x].dwRemotePort)
+              );
+    }
+
     return status;
 }
